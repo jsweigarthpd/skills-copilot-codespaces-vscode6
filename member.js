297dfefe4f8b6f2306fa4d0c6df0a6aa8f96d524
jsweigarthpd <153291693+jsweigarthpd@users.noreply.github.com>
2023-12-08 16:35:38 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var projects = document.getElementById("projects");
+
+  member.style.display = "block";
+  skills.style.display = "none";
+  projects.style.display = "none";
+}
