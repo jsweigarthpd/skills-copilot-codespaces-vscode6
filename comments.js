8076c487c4fefbc8408be5d3f77bc19c529674b1
jsweigarthpd <153291693+jsweigarthpd@users.noreply.github.com>
2023-12-08 16:47:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,28 +1,40 @@
-// create web server
-// set up routes
-// set up middleware
-// listen for requests
-// respond to requests
+// Create web server using Express
+const express = require('express');
+const router = express.Router();
 
-const express = require("express");
-const app = express();
-const path = require("path");
-const port = 3000;
+// Import data
+const data = require('../data/data.json');
 
-// import routes
-const comments = require("./routes/comments");
+// Import data from JSON file
+const { comments } = data;
 
-// set up middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// set up routes
-app.use("/api/comments", comments);
-
-// set up static files
-app.use(express.static(path.join(__dirname, "../public")));
-
-// listen for requests
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+// Set up route for comments page
+router.get('/comments', (req, res) => {
+    // Render comments page with data from JSON file
+    res.render('comments', {
+        comments,
+        pageTitle: 'Comments',
+        pageID: 'comments'
+    });
 });
+
+// Set up route for comment page
+router.get('/comments/:id', (req, res) => {
+    // Set up variables for comment ID and comment data
+    const comment = comments[req.params.id];
+    const name = comment.name;
+    const commentText = comment.comment;
+    const commentID = comment.id;
+
+    // Render comment page with data from JSON file
+    res.render('comment', {
+        name,
+        commentText,
+        commentID,
+        pageTitle: 'Comment',
+        pageID: 'comment'
+    });
+});
+
+// Export router
+module.exports = router;
